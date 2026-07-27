f46ce88236b5a0e0d5304ff6008f898bd84f79b3
diego huynh <hnam.d3@gmail.com>
2026-07-27 09:30:00 +0700

    feat: initialize feature store entities and yaml configuration

---
+++ feature_repository/entities.py
@@ -0,0 +1,8 @@
+from feast import Entity
+from feast.value_type import ValueType
+
+card_entity = Entity(
+    name="card_id",
+    value_type=ValueType.STRING,
+    description="Unique anonymized Payment Card / Account Identifier (card1)"
+)
